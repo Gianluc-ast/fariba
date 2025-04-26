ceefd730651aba18676259acbc2a8460cf52f9c6
Gianluca Astengo <gastengo98@gmail.com>
2025-04-25 18:03:05 -0700

    Avance

--- js/logos.js
+++ js/logos.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
     const logos = document.querySelector(".logos");
     const clone = logos.innerHTML;
-    logos.innerHTML += clone; // Duplica los logos para que la animación sea fluida
+    logos.innerHTML += clone; 
 });
